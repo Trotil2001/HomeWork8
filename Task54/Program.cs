fb8db00fefb8c428b0089385f5160c0ae28067df
Trotil_2001 <trotil_2001@mail.ru>
2022-09-28 21:02:18 +0800

    Задача 54

---
+++ Task54/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу,
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(0, 11);
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            //Console.Write(matrix[i, j] + " \t");
+            Console.Write("{0,5}", matrix[i, j]);
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int minElem = matrix[i, 0];
+        for (int k = 0;k < matrix.GetLength(1) - 1;k++)
+        {
+            for (int j = 0; j < (matrix.GetLength(1)) - 1; j++)
+            {
+                if (matrix[i, j] < matrix[i, j + 1])
+                {
+                    minElem = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                    matrix[i, j + 1] = minElem;
+                }
+            }
+        }
+    }
+}
+
+
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+Console.WriteLine("Исходный двумерный массив: ");
+PrintMatrix(matrix);
+ReleaseMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine("Конечный двумерный массив: ");
+PrintMatrix(matrix);
