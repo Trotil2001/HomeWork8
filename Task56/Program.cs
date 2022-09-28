2349f261f2a22279be61831197601ab7374a636b
Trotil_2001 <trotil_2001@mail.ru>
2022-09-28 21:02:27 +0800

    Задача 56

---
+++ Task56/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с 
+наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и 
+выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(0, 11);
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            //Console.Write(matrix[i, j] + " \t");
+            Console.Write("{0,5}", matrix[i, j]);
+        Console.WriteLine();
+    }
+}
+
+int ReleaseMatrix(int[,] matrix)
+{
+    int minSum=0, minSumIndex=-1;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sumRow = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumRow += matrix[i, j];
+        }
+        if (minSum == 0 || minSum>sumRow)
+        {
+            minSum = sumRow;
+            minSumIndex = i+1;
+        }
+        
+        Console.WriteLine($"Номер строки {i+1} Сумма: {sumRow};");
+    }
+    return minSumIndex;
+}
+
+
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+Console.WriteLine("Исходный двумерный массив: ");
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine("Номер строкти с наименьшей суммой элементов: " + ReleaseMatrix(matrix));
