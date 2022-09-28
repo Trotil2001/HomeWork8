fde568432cd433b2650e7cef80453a97e3cd0575
Trotil_2001 <trotil_2001@mail.ru>
2022-09-28 21:03:23 +0800

    Задача 61 Доп

---
+++ Task61Dop/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Дополнительная задача
+Задача 61: Вывести первые N строк треугольника Паскаля. 
+Сделать вывод в виде равнобедренного треугольника
+*/
+
+void Triangle(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int space = n; space > i; space--)
+            Console.Write(" ");
+
+        int num = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            Console.Write(num + " ");
+            num = num * (i - j) / j;
+        }
+        Console.WriteLine("");
+    }
+}
+
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Triangle(n);
+
+
